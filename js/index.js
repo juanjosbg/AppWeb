762473ce03c79c06789b5e650132a2b69663bca6
juanjosbg <juanjoseborrero95@gmail.com>
2022-10-26 16:46:57 -0500

    add react

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
-alert('hola');
-const root = document.getElementById(root);
-root.innerHTML = "Spacio Maker"
+//alert('Prueba');
+
+'use strict';
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+//
+
+
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(root));
